829654b4afedf7d2ec54107ad4157a8839875ee7
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-07 03:15:27 +0700

    Layout / styling for Block page

--- src/components/pages/BlockPage.vue
+++ src/components/pages/BlockPage.vue
@@ -4,20 +4,59 @@
       <back-button/>
       Block {{ block.height }}
     </h1>
-    <formatted-hash :hash='block.hash' :short='false'/>
-    {{ block.nonce }}
+    <div class="columns">
+      <div :class="labelClasses">
+        Hash
+      </div>
+      <div :class="dataClasses">
+        <formatted-hash :hash='block.hash' :short='false'/>
+      </div>
+    </div>
+    <div class="columns">
+      <div :class="labelClasses">
+        Nonce
+      </div>
+      <div :class="dataClasses">
+        {{ block.nonce }}
+      </div>
+    </div>
+    <div class="columns">
+      <div :class="labelClasses">
+        Mined on
+      </div>
+      <div :class="dataClasses">
+        <formatted-unix-time :time="block.time" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import FormattedHash from '../formatters/FormattedHash';
 import BackButton from '../misc/BackButton';
+import FormattedHash from '../formatters/FormattedHash';
+import FormattedUnixTime from '../formatters/FormattedUnixTime';
 
 export default {
   name: 'block-page',
   components: {
-    FormattedHash,
     BackButton,
+    FormattedHash,
+    FormattedUnixTime,
+  },
+  data() {
+    return {
+      dataClasses: [
+        'block',
+        'column',
+      ],
+      labelClasses: [
+        'block',
+        'column',
+        'is-one-quarter',
+        'has-text-right-tablet',
+        'label',
+      ],
+    };
   },
   computed: {
     block() {
@@ -29,3 +68,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.block.column {
+  padding-bottom: 0;
+  padding-top: 0;
+  margin-bottom: .75rem;
+}
+</style>
+
