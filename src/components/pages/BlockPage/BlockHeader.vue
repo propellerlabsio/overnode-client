6ba114d747023b3310210d981306391d8eca16ba
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-11 08:51:23 +0700

    Improve layout of block header

--- src/components/pages/BlockPage/BlockHeader.vue
+++ src/components/pages/BlockPage/BlockHeader.vue
@@ -1,62 +1,86 @@
 <template>
-  <div>
+  <div class="block-header">
     <!-- Hash -->
     <div class="columns">
-      <div :class="labelClasses">
-        Hash
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Hash
+        </div>
       </div>
-      <div :class="dataClasses">
-        <formatted-hash :hash='block.hash' :short='false'/>
+      <div class="column">
+        <div :class="dataClasses">
+          <formatted-hash :hash="block.hash" :shorten="false"/>
+        </div>
       </div>
     </div>
 
     <!-- Time -->
     <div class="columns">
-      <div :class="labelClasses">
-        Mined on
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Mined on
+        </div>
       </div>
-      <div :class="dataClasses">
-        <formatted-unix-time :time="block.time" />
+      <div class="column">
+        <div :class="dataClasses">
+          <formatted-unix-time :time="block.time" />
+        </div>
       </div>
     </div>
 
     <!-- Confirmations -->
     <div class="columns">
-      <div :class="labelClasses">
-        Confirmations
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Confirmations
+        </div>
       </div>
-      <div :class="dataClasses">
-        {{ block.confirmations }}
+      <div class="column">
+        <div :class="dataClasses">
+          {{ block.confirmations }}
+        </div>
       </div>
     </div>
 
     <!-- Size -->
     <div class="columns">
-      <div :class="labelClasses">
-        Size
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Size
+        </div>
       </div>
-      <div :class="dataClasses">
-        {{ block.size }}
+      <div class="column">
+        <div :class="dataClasses">
+          {{ block.size }}
+        </div>
       </div>
     </div>
 
     <!-- Interval -->
     <div class="columns">
-      <div :class="labelClasses">
-        Time since previous block
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Time since previous block
+        </div>
       </div>
-      <div :class="dataClasses">
-        <formatted-block-interval :interval="block.summary.interval" :humanize="false" />
+      <div class="column">
+        <div :class="dataClasses">
+          <formatted-block-interval :interval="block.summary.interval" :humanize="false" />
+        </div>
       </div>
     </div>
 
     <!-- Number of transactions -->
     <div class="columns">
-      <div :class="labelClasses">
-        Number of transactions
+      <div class="column no-padding-bottom is-2">
+        <div :class="labelClasses">
+          Number of transactions
+        </div>
       </div>
-      <div :class="dataClasses">
-        {{ block.summary.tx_count }}
+      <div class="column">
+        <div :class="dataClasses">
+          {{ block.summary.tx_count }}
+        </div>
       </div>
     </div>
   </div>
@@ -80,14 +104,9 @@ export default {
   data() {
     return {
       dataClasses: [
-        'block',
-        'column',
         'is-clipped',
       ],
       labelClasses: [
-        'block',
-        'column',
-        'is-one-quarter',
         'has-text-right-tablet',
         'label',
       ],
@@ -97,10 +116,13 @@ export default {
 </script>
 
 <style scoped>
-  .block.column {
-    padding-bottom: 0.25rem;
-    padding-top: 0.25rem;
-    /* margin-top: .25rem; */
-    margin-bottom: .25rem;
+
+  .block-header {
+    margin-bottom: 15px;
   }
+
+  .no-padding-bottom {
+    padding-bottom: 0;
+  }
+
 </style>
