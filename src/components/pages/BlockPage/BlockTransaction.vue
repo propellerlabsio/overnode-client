d73eace2c577e97b70b8376a263a888f4af61eef
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-11 15:55:06 +0700

    Drill down to transaction detail (scaffolding)

---
+++ src/components/pages/BlockPage/BlockTransaction.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <a class="button" @click="$emit('back')">
+      <span class="icon">
+        <i class="fa fa-chevron-left"></i>
+      </span>
+      <span>
+        Back
+      </span>
+    </a>
+    <h2 class="subtitle">Transaction details</h2>
+    <table class="table">
+      <tbody>
+        <tr>
+          <th>
+            No.
+          </th>
+          <td>
+            {{ index }}
+          </td>
+        </tr>
+        <tr>
+          <th>
+            Id
+          </th>
+          <td>
+            <formatted-hash class="is-hidden-tablet" :hash='transaction.txid' :shorten="true"/>
+            <formatted-hash class="is-hidden-mobile" :hash='transaction.txid' :shorten="false"/>
+          </td>
+        </tr>
+        <tr>
+          <th>
+            Inputs
+          </th>
+          <td>
+            {{ transaction.inputs.length }}
+          </td>
+        </tr>
+        <tr>
+          <th>
+            Outputs
+          </th>
+          <td>
+            {{ transaction.outputs.length }}
+          </td>
+        </tr>
+        <tr>
+          <th>
+            Size
+          </th>
+          <td>
+            {{ transaction.size }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import FormattedHash from '../../formatters/FormattedHash';
+
+export default {
+  name: 'block-transaction',
+  props: {
+    transaction: Object,
+    index: Number,
+  },
+  components: {
+    FormattedHash,
+  },
+};
+</script>
+
+<style scoped>
+  th {
+    width: 100px;
+  }
+</style>
--- src/components/pages/BlockPage/BlockTransactions.vue
+++ src/components/pages/BlockPage/BlockTransactions.vue
@@ -1,63 +1,86 @@
 <template>
   <div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>
-            No.
-          </th>
-          <th>
-            Id
-          </th>
-          <th>
-            Inputs
-          </th>
-          <th>
-            Outputs
-          </th>
-          <th>
-            Size
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(transaction, index) in transactions" :key="transaction.txid">
-          <td>
-            {{ index + (page.limit * (page.current - 1)) }}
-          </td>
-          <td>
-            <formatted-hash class="is-hidden-tablet" :hash='transaction.txid' :shorten="true"/>
-            <formatted-hash class="is-hidden-mobile" :hash='transaction.txid' :shorten="false"/>
-          </td>
-          <td class="has-text-right">
-            {{ transaction.inputs.length }}
-          </td>
-          <td class="has-text-right">
-            {{ transaction.outputs.length }}
-          </td>
-          <td class="has-text-right">
-            {{ transaction.size }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <pager
-      :disabled="loading"
-      :current-page="page.current"
-      :last-page="page.last"
-      @previous="gotoPage(page.current - 1)"
-      @next="gotoPage(page.current + 1)"
-      @goto="gotoPage"/>
+    <!-- Selected Transaction -->
+    <block-transaction
+      v-if="selectedTransaction"
+      :transaction="selectedTransaction"
+      :index="selectedIndex"
+      @back="selectedTransaction = null"/>
+
+    <!-- Transactions table -->
+    <div v-else>
+      <table class="table is-striped is-hoverable">
+        <thead>
+          <tr>
+            <th>
+              No.
+            </th>
+            <th>
+              Id
+            </th>
+            <th>
+              Inputs
+            </th>
+            <th>
+              Outputs
+            </th>
+            <th>
+              Size
+            </th>
+            <th>
+              <!-- Nav to tx -->
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(transaction, index) in transactions" :key="transaction.txid"
+            @click="selectTransaction(index)">
+            <td>
+              {{ getTransactionIndex(index) }}
+            </td>
+            <td>
+              <formatted-hash class="is-hidden-tablet" :hash='transaction.txid' :shorten="true"/>
+              <formatted-hash class="is-hidden-mobile" :hash='transaction.txid' :shorten="false"/>
+            </td>
+            <td class="has-text-right">
+              {{ transaction.inputs.length }}
+            </td>
+            <td class="has-text-right">
+              {{ transaction.outputs.length }}
+            </td>
+            <td class="has-text-right">
+              {{ transaction.size }}
+            </td>
+            <td class="has-text-centered">
+              <a class="button is-white">
+                <span class="icon">
+                  <i class="fa fa-chevron-right"></i>
+                </span>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <pager
+        :disabled="loading"
+        :current-page="page.current"
+        :last-page="page.last"
+        @previous="gotoPage(page.current - 1)"
+        @next="gotoPage(page.current + 1)"
+        @goto="gotoPage"/>
+    </div>
   </div>
 </template>
 
 <script>
+import BlockTransaction from './BlockTransaction';
 import FormattedHash from '../../formatters/FormattedHash';
 import Pager from '../../misc/Pager';
 
 export default {
   name: 'block-transactions',
   components: {
+    BlockTransaction,
     FormattedHash,
     Pager,
   },
@@ -67,6 +90,8 @@ export default {
   data() {
     return {
       loading: true,
+      selectedTransaction: null,
+      selectedIndex: null,
     };
   },
   computed: {
@@ -78,6 +103,13 @@ export default {
     },
   },
   methods: {
+    getTransactionIndex(indexOnPage) {
+      return indexOnPage + (this.page.limit * (this.page.current - 1));
+    },
+    selectTransaction(indexOnPage) {
+      this.selectedTransaction = this.transactions[indexOnPage];
+      this.selectedIndex = this.getTransactionIndex(indexOnPage);
+    },
     async gotoPage(pageNumber) {
       this.loading = true;
       await this.$store.dispatch('blocks/gotoTransactionsPage', pageNumber);
