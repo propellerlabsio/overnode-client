bb074cabe63c0ab794647656d63cacb1f8047ff6
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-13 23:31:27 +0700

    Make peers page responsive

--- src/components/pages/PeersPage.vue
+++ src/components/pages/PeersPage.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
     <page-title title="Peers"/>
-    <table class="table">
+    <table class="table is-striped is-hoverable">
       <thead>
         <tr>
           <th class="has-text-centered">Id</th>
-          <th class="has-text-centered">Address</th>
+          <th class="has-text-centered is-hidden-mobile">Address</th>
+          <th class="has-text-centered is-hidden-mobile">Version</th>
           <th class="has-text-centered">Location</th>
-          <th class="has-text-centered">Received</th>
-          <th class="has-text-centered">Sent</th>
+          <th class="has-text-centered is-hidden-touch">Received</th>
+          <th class="has-text-centered is-hidden-touch">Sent</th>
+          <th class="has-text-centered is-hidden-touch">Ping</th>
+          <th class="has-text-centered">Direction</th>
+          <th>
+            <!-- Nav to peer -->
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="peer in peers" v-bind:key="peer.id">
           <td>{{ peer.id }}</td>
-          <td>{{ peer.addr }}</td>
+          <td class="is-hidden-mobile">{{ peer.addr }}</td>
+          <td class="is-hidden-mobile">{{ peer.subver }}</td>
           <td>
             <span v-if="peer.location.city">
               {{ peer.location.city }},
@@ -24,11 +31,24 @@
             </span>
             {{ peer.location.country }}
           </td>
-          <td class="has-text-right">
+          <td class="has-text-right is-hidden-touch">
             {{ peer.bytesrecv }}
           </td>
-          <td class="has-text-right">
+          <td class="has-text-right is-hidden-touch">
             {{ peer.bytessent }}
+          </td>
+          <td class="has-text-right is-hidden-touch">
+            {{ pingToMilliseconds(peer.pingtime) }} ms
+          </td>
+          <td class="has-text-centered">
+            {{ inbound ? 'in' : 'out' }}
+          </td>
+          <td class="has-text-centered">
+            <a class="button is-white is-small">
+              <span class="icon">
+                <i class="fa fa-chevron-right"></i>
+              </span>
+            </a>
           </td>
         </tr>
       </tbody>
@@ -51,6 +71,11 @@ export default {
   computed: {
     peers() {
       return this.$store.state.peers.current;
+    },
+  },
+  methods: {
+    pingToMilliseconds(ping) {
+      return Math.round(ping * 1000);
     },
   },
 };
