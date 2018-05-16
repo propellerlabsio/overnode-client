4b86b2e1a02f60b3951551f4dfe83aee6c1d4025
Chhunly Bin <chhunly.bin@gamil.com>
2018-05-16 15:58:02 +0700

    Add table to a sync error

--- src/components/pages/AdminPage/SyncErrors.vue
+++ src/components/pages/AdminPage/SyncErrors.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>Sync errors page coming soon</p>
+<div>
+<table class="table">
+  <thead>
+    <tr>
+      <th><abbr title="Played">Height</abbr></th>
+      <th><abbr title="Played">Name</abbr></th>
+      <th><abbr title="Played">Message</abbr></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1001- {{synError}}</td>
+      <td>Test1</td>
+      <td>Testing this error message</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </template>
+<script>
+export default {
+  name: 'syn',
+  components: {
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>>
